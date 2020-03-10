e1a29b66de80f0bc56895fea34c44decaa0de785
Chris Banta <chrisbanta1@gmail.com>
2020-03-10 19:30:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,7 +5,7 @@ def find_element_index(array, value_to_find)
        return array[index]
       else
       index = index + 1
-
+end
 end
 end
 
@@ -14,6 +14,7 @@ def find_max_value(array)
   sorted_array = array.sort
   return sorted_array[-1]
 end
+
 
 def find_min_value(array)
   sorted_array = array.sort
