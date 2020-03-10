41d1c6b04b8b07d2c27e47d45dc6a8e159d0f7eb
Chris Banta <chrisbanta1@gmail.com>
2020-03-10 19:00:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
   index = 0
-  while value_to_find != array[index] do
-      index = index + 1
+  while value_to_find == array[index] do
+
       return array[index]
+      index = index + 1
 end
 end
 
