db13785f58ce8e734a9316154add22de2f3ae7d6
Chris Banta <chrisbanta1@gmail.com>
2020-03-10 18:35:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,8 @@
 def find_element_index(array, value_to_find)
-  length = array.length
-  length.times do |index|
-  value_to_find == array[index]
-  return array[index]
-
-  end
+  while value_to_find != array[index] do
+      index = index + 1
+      return array[index]
+end
 
 
 def find_max_value(array)
