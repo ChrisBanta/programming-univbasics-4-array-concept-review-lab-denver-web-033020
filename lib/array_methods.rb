da4c7fa261a90cd19890f8baed39c62aa95b6236
Chris Banta <chrisbanta1@gmail.com>
2020-03-10 18:17:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   length = array.length
   length.times do |index|
   value_to_find == array[index]
-  return array[index]  
+  return array[index]
 
   end
   
